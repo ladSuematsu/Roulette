f4dafde5b393b971c69310167527f4e08902e8b5
ladSuematsu <suematsu01@gmail.com>
2015-09-27 18:39:15 -0300

    Implements transitions and background animation for the Result Dialog.

--- app/src/main/java/ladsoft/roulette/fragment/ResultDialog.java
+++ app/src/main/java/ladsoft/roulette/fragment/ResultDialog.java
@@ -5,10 +5,16 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ladsoft.roulette.R;
@@ -26,6 +32,7 @@ public class ResultDialog extends DialogFragment{
     private String mPlace;
     private String mWeekday;
     private PlaceHistory mPlaceHistory;
+    private AnimationDrawable mAnimationDrawable;
 
     private OnFragmentInteractionListener mListener;
 
@@ -77,7 +84,18 @@ public class ResultDialog extends DialogFragment{
 
         View view = inflater.inflate(R.layout.fragment_result, null);
         mResultText = (TextView) view.findViewById(R.id.fragment_result_text);
+
+        ImageView imageViewAnimation = (ImageView) view.findViewById(R.id.fragment_result_animation);
+        imageViewAnimation.setBackgroundResource(R.drawable.explosion_animation);
+        imageViewAnimation.setVisibility(View.VISIBLE);
+
+
+        mAnimationDrawable= (AnimationDrawable) imageViewAnimation.getBackground();
+        mAnimationDrawable.setOneShot(true);
+
+
         mResultText.setText(mPlaceHistory.getPlace());
+
 
         builder.setView(view)
             .setNegativeButton(R.string.message_cancel, new DialogInterface.OnClickListener() {
@@ -94,8 +112,42 @@ public class ResultDialog extends DialogFragment{
             });
 
 
-        //return super.onCreateDialog(savedInstanceState);
-        return builder.create();
+        Dialog dialog = builder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                mAnimationDrawable.start();
+                //mAnimationDrawable.setVisible(true, true);
+            }
+        });
+
+        return dialog;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        getDialog().getWindow()
+//                .getAttributes().windowAnimations=R.style.DialogAnimation;
+
+        Window dialogWindow = getDialog().getWindow();
+
+        dialogWindow.setWindowAnimations(R.style.DialogAnimation);
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        WindowManager.LayoutParams lytParams = dialogWindow.getAttributes();
+        lytParams.verticalMargin = 0;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
     }
 
     /**
